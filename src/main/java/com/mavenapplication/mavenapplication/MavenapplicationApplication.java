db58d6f470c7994cfedff641f636033b75f3166b
jadhav1994 <premjadhav.jadhav@gmail.com>
2022-10-19 16:45:49 +0530

    This is my maven file

---
+++ src/main/java/com/mavenapplication/mavenapplication/MavenapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.mavenapplication.mavenapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mavenapplication/mavenapplication/MavenapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavenapplication.mavenapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
